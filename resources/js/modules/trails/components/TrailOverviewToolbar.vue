525d75ce757743e02bca8f12f1bb580a945c0d9d
Kamil <kamz8@wp.pl>
2024-08-31 16:52:00 +0200

    add loaclization breadcrumbs for displaying trail regions

--- resources/js/modules/trails/components/TrailOverviewToolbar.vue
+++ resources/js/modules/trails/components/TrailOverviewToolbar.vue
@@ -1,21 +1,35 @@
 <template>
-<v-breadcrumbs >
-    <v-breadcrumbs-item>Polska</v-breadcrumbs-item>
-    <v-breadcrumbs-divider><v-icon icon="mdi-circle-small"/></v-breadcrumbs-divider>
-    <v-breadcrumbs-item>Województwo</v-breadcrumbs-item>
-    <v-breadcrumbs-divider><v-icon icon="mdi-circle-small"/></v-breadcrumbs-divider>
-    <v-breadcrumbs-item>Region / miasto</v-breadcrumbs-item>
-</v-breadcrumbs>
+    <v-breadcrumbs>
+        <template v-for="(region, index) in sortedRegions" :key="region.id">
+            <v-breadcrumbs-item>{{ region.name }}</v-breadcrumbs-item>
+            <v-breadcrumbs-divider v-if="index < sortedRegions.length - 1">
+                <v-icon icon="mdi-circle-small"/>
+            </v-breadcrumbs-divider>
+        </template>
+    </v-breadcrumbs>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-    name: "TrailOverviewToolbar"
+    name: "TrailOverviewToolbar",
+    computed: {
+        ...mapGetters({
+            currentTrail: 'trails/currentTrail'
+        }),
+        sortedRegions() {
+            if (!this.currentTrail || !this.currentTrail.regions) {
+                return [];
+            }
+            return this.currentTrail.regions.sort((a, b) => {
+                const order = ['country', 'state', 'city', 'geographic_area'];
+                return order.indexOf(a.type) - order.indexOf(b.type);
+            });
+        }
+    }
 }
 </script>
 
-
-
 <style scoped>
-
 </style>
