8ee68a15f8b8707fb948bbdb2e696d974d7d9aee
Kamil <kamz8@wp.pl>
2024-10-05 20:38:46 +0200

    render.com dockerfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\ReverseGeocodingService;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
