b5a16cda1398be6b3c654782f459ff39446ee336
Kamil <kamz8@wp.pl>
2024-10-07 13:19:46 +0200

    render.com dockerfile

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,8 +4,6 @@ echo "Running composer"
 
 composer install --no-dev --working-dir=/var/www/html --prefer-dist --no-scripts --no-progress --no-interaction
 
-RUN npm ci && npm run build
-
 echo "Caching config..."
 php artisan config:cache
 echo "Caching routes..."
@@ -14,3 +12,6 @@ php artisan route:cache
 # Upewnienie się, że po buildzie mamy odpowiednie uprawnienia
 echo "Setting permissions for storage and cache"
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+npm ci && npm run build
+
