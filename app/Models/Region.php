7b810dea518e9e502fccd5efba9f3c0df43ea704
Kamil <kamz8@wp.pl>
2024-10-28 14:48:20 +0100

    Add images to region model

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use App\Enums\RegionType;
 use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
