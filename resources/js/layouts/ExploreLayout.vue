e789496f721690bcc828bd392fac56d1dfa726c0
Kamil <kamz8@wp.pl>
2024-11-20 14:19:11 +0100

    Fix bug with layout. Fixed a layout bug that caused extra space bottom to be added below the app view.

--- resources/js/layouts/ExploreLayout.vue
+++ resources/js/layouts/ExploreLayout.vue
@@ -1,37 +1,32 @@
 <template>
-    <v-app>
-        <v-container fluid class="d-flex flex-column pa-0 full-height">
-            <!-- Navbar -->
-            <Navbar />
+    <!-- Navbar -->
+    <Navbar/>
 
-            <!-- Toolbar for search and filters -->
-            <v-app-bar elevation="0">
-                <router-view name="toolbar" />
-            </v-app-bar>
+    <!-- Toolbar for search and filters -->
+    <v-app-bar app elevation="0">
+        <router-view name="toolbar"/>
+    </v-app-bar>
 
-            <v-row class="d-flex flex-grow-1">
-                <!-- Sidebar Panel -->
-                <v-navigation-drawer v-model="drawer" app clipped width="400" elevation="2" class="d-flex flex-column" >
-                    <router-view name="sidebar"></router-view>
-                    <v-btn
-                        icon
-                        @click="toggleDrawer"
-                        class="drawer-toggle-btn"
-                        size="s"
-                    >
-                        <v-icon>{{ drawer ? 'mdi-chevron-left' : 'mdi-chevron-right' }}</v-icon>
-                    </v-btn>
-                </v-navigation-drawer>
+    <!-- Sidebar Panel -->
+    <v-navigation-drawer v-model="drawer" app clipped width="400" elevation="2" class="d-flex flex-column">
+        <router-view name="sidebar"></router-view>
+        <v-btn
+            icon
+            @click="toggleDrawer"
+            class="drawer-toggle-btn"
+            size="s"
+        >
+            <v-icon>{{ drawer ? 'mdi-chevron-left' : 'mdi-chevron-right' }}</v-icon>
+        </v-btn>
+    </v-navigation-drawer>
 
-                <!-- Main Content -->
-                <v-main class="flex-grow-1">
-                    <v-container fluid class="pa-0 d-flex flex-column" style="height: calc(100vh - 64px - 56px);">
-                        <router-view name="main"></router-view>
-                    </v-container>
-                </v-main>
-            </v-row>
+    <!-- Main Content -->
+    <v-main app class="flex-grow-1">
+        <v-container app fluid class="pa-0 d-flex flex-column" style="height: calc(100vh - 64px - 66px);">
+            <router-view name="main"></router-view>
         </v-container>
-    </v-app>
+    </v-main>
+
 </template>
 
 <script>
