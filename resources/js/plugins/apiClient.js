610d567900e151f9acc8137d86f077839ff44024
Kamil <kamz8@wp.pl>
2025-03-16 22:41:48 +0100

    fix import const VITE_API_URL

--- resources/js/plugins/apiClient.js
+++ resources/js/plugins/apiClient.js
@@ -1,35 +1,36 @@
-import axios from 'axios'
+import axios from 'axios';
+
+// Ustawienie wartości domyślnej dla VITE_API_URL
+const apiUrl = import.meta.env.VITE_API_URL || 'https://wartkinurt.pl';
+
+// Budowanie pełnego URL do API
+const baseURL = `${apiUrl}/api/v1`;
 
 const apiClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+    baseURL: baseURL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'X-Client-Type': 'web'
-    }
-})
+        'X-Client-Type': 'web',
+    },
+});
 
 apiClient.interceptors.request.use(
     (config) => {
-
-        // console.log(import.meta.env.VITE_API_URL)
-        // Upewniamy się, że config.headers istnieje
         config.headers = config.headers || {};
-
-        // Wymuszamy nagłówek X-Client-Type
         config.headers['X-Client-Type'] = 'web';
 
-        const token = localStorage.getItem('token')
+        const token = localStorage.getItem('token');
         if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`
+            config.headers['Authorization'] = `Bearer ${token}`;
         }
 
-        return config
+        return config;
     },
     (error) => {
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
-)
+);
 
-export default apiClient
+export default apiClient;
