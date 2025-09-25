f9f07ef40e0d69f30d8bf5af26ca315a21da70d3
Kamil <kamz8@wp.pl>
2025-09-26 01:29:05 +0200

    Chore add acl boilerplate

--- resources/js/dashboard/main.js
+++ resources/js/dashboard/main.js
@@ -4,6 +4,7 @@ import router from './router/index.js'
 import store from './store/index.js'
 import vuetify from './plugins/vuetify.js'
 import axios from './plugins/axios.js'
+import { createPermissionService } from './services/PermissionService.js'
 
 // CSS imports
 import 'vuetify/styles'
@@ -21,8 +22,20 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['X-Client-Type'] = 'web';
+
+// Initialize permission service
+const permissionService = createPermissionService(store)
+
 // Global properties
 app.config.globalProperties.$http = axios
+app.config.globalProperties.$permissions = permissionService
+app.config.globalProperties.$can = (permission) => permissionService.can(permission)
+app.config.globalProperties.$canAny = (permissions) => permissionService.canAny(permissions)
+app.config.globalProperties.$canAll = (permissions) => permissionService.canAll(permissions)
+app.config.globalProperties.$hasRole = (role) => permissionService.hasRole(role)
+app.config.globalProperties.$hasAnyRole = (roles) => permissionService.hasAnyRole(roles)
+app.config.globalProperties.$isAdmin = () => permissionService.isAdmin()
+app.config.globalProperties.$isSuperAdmin = () => permissionService.isSuperAdmin()
 
 // Initialize auth
 store.dispatch('auth/initialize')
--- resources/js/dashboard/store/index.js
+++ resources/js/dashboard/store/index.js
@@ -2,12 +2,14 @@ import { createStore } from 'vuex'
 import auth from '../modules/auth/store/auth.js'
 import ui from './modules/ui.js'
 import users from '../modules/users/store/index.js'
+import roles from '../modules/roles/store/index.js'
 
 const store = createStore({
   modules: {
     auth,
     ui,
-    users
+    users,
+    roles
   },
   strict: process.env.NODE_ENV !== 'production'
 })
